3d23e76573f9408ea8b12497c03e3a49576a5f3f
MWillian <Matheuswno02@gmail.com>
2026-03-21 20:58:50 -0300

    Criação de biblioteca de classe de exceções. Criação de classe pai para lançamento de erros.

---
+++ MwSolucoesApi/MwSolucoes.Exception/ExceptionBase/MwSolucoesException.cs
@@ -0,0 +1,9 @@
+﻿namespace MwSolucoes.Exception.ExceptionBase
+{
+    public abstract class MwSolucoesException : SystemException
+    {
+        protected MwSolucoesException(string message) : base(message) { }
+        public abstract int StatusCode { get; }
+        public abstract List<string> GetErrors();
+    }
+}
