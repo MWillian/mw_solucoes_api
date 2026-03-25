3a2e8a45b3104f6a9611e7f94efcf2905334d33a
MWillian <Matheuswno02@gmail.com>
2026-03-25 19:08:11 -0300

    feat07/ Implementado o filtro de exceções e criada classe genérica de resposta de erro.

---
+++ MwSolucoesApi/MwSolucoes.Communication/Responses/ResponseError.cs
@@ -0,0 +1,15 @@
+﻿namespace MwSolucoes.Communication.Responses
+{
+    public class ResponseError
+    {
+        public List<string> ErrorsMessages { get; set; }
+        public ResponseError(string errorMessage)
+        {
+            ErrorsMessages = new List<string>() { errorMessage };
+        }
+        public ResponseError(List<string> errorMessages)
+        {
+            ErrorsMessages = errorMessages;
+        }
+    }
+}
---
+++ MwSolucoesApi/MwSolucoesApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MwSolucoes.Communication.Responses;
+using MwSolucoes.Exception.ExceptionBase;
+using MwSolucoes.Exception.ResouceErrors;
+
+namespace MwSolucoes.Api.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is MwSolucoesException)
+            {
+                HandleProjectException(context);
+            }
+            else
+            {
+                ThrowUnknownError(context);
+            }
+        }
+        private void HandleProjectException(ExceptionContext context)
+        {
+            var exception = context.Exception as MwSolucoesException;
+            var errorResponse = new ResponseError(exception!.GetErrors());
+
+            context.HttpContext.Response.StatusCode = exception.StatusCode;
+            context.Result = new ObjectResult(errorResponse);
+        }
+        private void ThrowUnknownError(ExceptionContext context)
+        {
+            var errorResponse = new ResponseError(ResouceErrorMessages.INTERNAL_ERROR);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(errorResponse);
+        }
+    }
+}
